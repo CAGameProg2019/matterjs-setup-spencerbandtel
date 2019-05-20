61dddcaa0f03b69f1b8255228e6faf1adae6e8e3
spencerbandtel <46567079+spencerbandtel@users.noreply.github.com>
2019-05-20 11:33:43 -0400

    It works

--- main.js
+++ main.js
@@ -11,13 +11,24 @@ let render = Render.create({
 });
 Engine.run(engine);
 Render.run(render);
- 
-let ball=Bodies.circle(200, 10, 40);
-let floor=Bodies.trapezoid(340,300,500,100,.9,{isStatic: true});
-let myCar=Composites.car(390, 0, 100, 30, 40);
-let myCradle=Composites.newtonsCradle(600, 100, 5, 10, 160);
-World.add(engine.world, [ball,floor,myCar,myCradle]);
- 
+
+var particleOptions = {
+        friction: 0.10,
+        frictionStatic: 0.1,
+        render: { visible: true }
+    };
+
+ let ball=Bodies.circle(300, 200, 40);
+ let ball2=Bodies.circle(500, 200, 40);
+ //let myCar=Composites.car(390, 0, 100, 30, 40);
+ //let chain = Composites.chain(myCar, 5, 5, 200, 200);
+ let floor=Bodies.trapezoid(400,400,300,100,.9,{isStatic: true});
+ Matter.Body.rotate(floor,(3.14));
+let blanket = Composites.softBody(200,50, 20, 20, 2, 2, true, 10, particleOptions);
+//let myCradle=Composites.newtonsCradle(200, 100, 5, 10, 160);
+World.add(engine.world, [ball,ball2,blanket,floor]);
+
+
 let world = engine.world;
 let Mouse= Matter.Mouse;
 let MouseConstraint=Matter.MouseConstraint;
